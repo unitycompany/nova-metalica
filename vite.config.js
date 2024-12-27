c675a51cba08127e834c5b7ff7f7ebc2a84cbbc5
unitycompany <equipe@unitycompany.com.br>
2024-12-27 09:57:07 -0300

    add

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-     rollupOptions: {
-       external: [
-          "/assets/index-BHhcukG4.js" 
-       ],
-     },
-  },
-  plugins: [react()],
-  base: "/", 
+  // Troque "meu-portfolio-react" pelo nome do seu repositório
+  base: "/nova-metalica/",
+  plugins: [react()]
 });
