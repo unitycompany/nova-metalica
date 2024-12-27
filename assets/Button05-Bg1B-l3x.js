00ace82684c028dfb3c6f1f54b63d82011f31373
unitycompany <equipe@unitycompany.com.br>
2024-12-27 09:57:32 -0300

    add

---
+++ assets/Button05-Bg1B-l3x.js
@@ -0,0 +1,9 @@
+import{d as t,j as o}from"./index-DxlpPnnr.js";const e=t.button`
+    color: var(--color--white);
+    background: linear-gradient(45deg, var(--color--blue), var(--color--dark--blue));
+    padding: 12px 20px;
+    border-radius: 12px;
+    font-weight: 400;
+    cursor: pointer;
+    font-size: 14px;
+`,a=({text:r})=>o.jsx(o.Fragment,{children:o.jsx(e,{onClick:()=>document.getElementById("contactForm").scrollIntoView({behavior:"smooth"}),children:r})});export{a as B};
---
+++ assets/Inicial-BxHoKlK5.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DxlpPnnr.js";const t=()=>r.jsx(r.Fragment,{});export{t as default};
